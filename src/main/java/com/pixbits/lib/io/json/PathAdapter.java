3d38325f369541cf82e1f29367ed7b7eef9deb82
Jack <jack@pixbits.com>
2017-03-14 14:48:55 +0100

    added json package

---
+++ src/main/java/com/pixbits/lib/io/json/PathAdapter.java
@@ -0,0 +1,26 @@
+package com.pixbits.lib.io.json;
+
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+
+public class PathAdapter implements JsonAdapter<Path>
+{
+  @Override
+  public Path deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException
+  {
+    String string = context.deserialize(element, String.class);
+    return Paths.get(string);
+  }
+
+  @Override
+  public JsonElement serialize(Path src, Type type, JsonSerializationContext context)
+  {
+    return context.serialize(src.toString());
+  }
+}
