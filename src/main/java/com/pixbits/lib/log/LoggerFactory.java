7976ebd67f9a39becb772532c5ddc300af057894
Jack <jack@pixbits.com>
2017-03-15 22:44:07 +0100

    added null logger to LoggerFactory

--- src/main/java/com/pixbits/lib/log/LoggerFactory.java
+++ src/main/java/com/pixbits/lib/log/LoggerFactory.java
@@ -5,10 +5,14 @@ public interface LoggerFactory
 {
   Logger build(LogScope scope);
   
+  
   public static final LoggerFactory STDOUT = s -> new StdoutLogger(s);
-  
   public static final LoggerFactory DEFAULT = STDOUT;
-  
+  public static final LoggerFactory NULL_FACTORY = s -> new Logger(s) {
+    @Override
+    protected <T extends LogAttribute> void doLog(Log type, String message, T attribute) { }
+  };
+    
   public static class BufferLoggerFactory implements LoggerFactory
   {
     private final LogBuffer buffer;
