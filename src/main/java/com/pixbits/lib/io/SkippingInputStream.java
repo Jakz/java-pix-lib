8d442c30df7ac91a42a9e2ef3e1a34a7bdbb6663
Jack <jack@pixbits.com>
2017-09-03 19:07:27 +0200

    added SkippingInputStream

---
+++ src/main/java/com/pixbits/lib/io/SkippingInputStream.java
@@ -0,0 +1,54 @@
+package com.pixbits.lib.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SkippingInputStream extends InputStream implements AutoCloseable
+{
+  private final InputStream is;
+  private final int bytesToSkip;
+  private int toSkip;
+  
+  public SkippingInputStream(InputStream is, int bytesToSkip)
+  {
+    this.is = is;
+    this.bytesToSkip = bytesToSkip;
+    this.toSkip = bytesToSkip;
+  }
+  
+  private void skipToStart() throws IOException
+  {
+    if (toSkip > 0)
+    {
+      byte[] skip = new byte[toSkip];
+      toSkip -= is.read(skip);
+    }
+  }
+  
+  @Override
+  public int read() throws IOException
+  {
+    if (toSkip > 0) skipToStart();
+    return is.read();   
+  }
+  
+  @Override
+  public int read(byte[] buffer) throws IOException
+  {
+    if (toSkip > 0) skipToStart();
+    return is.read(buffer);
+  }
+  
+  @Override
+  public int read(byte[] buffer, int s, int l) throws IOException
+  {
+    if (toSkip > 0) skipToStart();
+    return is.read(buffer, s, l);
+  }
+  
+  @Override
+  public void close() throws IOException
+  {
+    is.close();
+  }
+}
