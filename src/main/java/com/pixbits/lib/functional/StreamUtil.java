0ea238d8b5ca20e64c0dd431a7ac24ee2b1217c3
Jack <jack@pixbits.com>
2018-07-03 15:09:28 +0200

    added assemble stream util function
    which composes multiple elements in a list of elements of specified size

--- src/main/java/com/pixbits/lib/functional/StreamUtil.java
+++ src/main/java/com/pixbits/lib/functional/StreamUtil.java
@@ -1,7 +1,13 @@
 package com.pixbits.lib.functional;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -31,4 +37,57 @@ public class StreamUtil
   public static <T> Predicate<T> not(Predicate<T> t) {
     return t.negate();
   }
+  
+  public static class ConsecutiveSpliterator<T> implements Spliterator<List<T>>
+  {
+    private final Spliterator<T> wrappedSpliterator;
+    private final int n;
+    private final Deque<T> deque;
+    private final Consumer<T> dequeConsumer;
+
+    public ConsecutiveSpliterator(Spliterator<T> wrappedSpliterator, int n)
+    {
+      this.wrappedSpliterator = wrappedSpliterator;
+      this.n = n;
+      this.deque = new ArrayDeque<>();
+      this.dequeConsumer = deque::addLast;
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super List<T>> action)
+    {
+      deque.pollFirst();
+      fillDeque();
+      if (deque.size() == n)
+      {
+        List<T> list = new ArrayList<>(deque);
+        action.accept(list);
+        return true;
+      } 
+      else
+        return false;
+    }
+
+    private void fillDeque() 
+    {
+      while (deque.size() < n && wrappedSpliterator.tryAdvance(dequeConsumer));
+    }
+
+    @Override
+    public Spliterator<List<T>> trySplit() { return null; }
+
+    @Override
+    public long estimateSize() { return wrappedSpliterator.estimateSize(); }
+
+    @Override
+    public int characteristics() { return wrappedSpliterator.characteristics(); }
+  }
+  
+  public static <E> Stream<List<E>> assemble(Stream<E> stream, int n) 
+  {
+    Spliterator<E> spliterator = stream.spliterator();
+    Spliterator<List<E>> wrapper = new ConsecutiveSpliterator<>(spliterator, n);
+    return StreamSupport.stream(wrapper, false);
+  }
+  
 }
